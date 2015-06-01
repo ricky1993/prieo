ec51d4c8aef98d8ad67e649f66103905421be993
Byphoenix <446596010@qq.com>
2015-06-01 17:54:30 +0800

    更改
    更改了GEMFILE； 更改了APPLICATION； 增加IMAGE，JS，CSS 文件，把
    config/initials/assert进行添加，把原来的application.html.erb 改成2f放在原来文件内

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery-2.1.4.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( One.css )
