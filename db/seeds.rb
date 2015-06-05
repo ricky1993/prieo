7524610248e1212ddc94d55a4aafd1b0c83cad1d
Byphoenix <446596010@qq.com>
2015-06-05 09:56:48 +0800

    fix user login

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,36 +9,67 @@
 Product.delete_all
 Product.create(title: 'CoffeeScript',
                description:
-                   %{<p>
+                   %{
         CoffeeScript is JavaScript done right. It provides all of JavaScript's
-	functionality wrapped in a cleaner, more succinct syntax. In the first
-	book on this exciting new language, CoffeeScript guru Trevor Burnham
-	shows you how to hold onto all the power and flexibility of JavaScript
-	while writing clearer, cleaner, and safer code.
-      </p>},
+	functionality wrapped in a cleaner,
+      },
                image_url:   '5.jpg',
                price: 36.00)
 # . . .
 Product.create(title: 'Programming Ruby 1.9 & 2.0',
                description:
-                   %{<p>
-        Ruby is the fastest growing and most exciting dynamic language
-        out there. If you need to get working programs delivered fast,
-        you should add Ruby to your toolbox.
-      </p>},
+                   %{
+        CoffeeScript is JavaScript done right. It provides all of JavaScript's
+	functionality wrapped in a cleaner,
+
+      },
                image_url: '6.jpg',
                price: 49.95)
 # . . .
 
-Product.create(title: 'Rails Test Prescriptions',
+Product.create(title: 'Rails T123est Prescriptions',
                description:
-                   %{<p>
-        <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
-        Rails applications, covering Test-Driven Development from both a
-        theoretical perspective (why to test) and from a practical perspective
-        (how to test effectively). It covers the core Rails testing tools and
-        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
-        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
-      </p>},
+                   %{
+        CoffeeScript is JavaScript done right. It provides all of JavaScript's
+	functionality wrapped in a cleaner,
+
+     },
                image_url: '7.jpg',
+               price: 34.95)
+
+# . . .
+
+Product.create(title: 'Rails 123Test Prescriptions',
+               description:
+                   %{
+        CoffeeScript is JavaScript done right. It provides all of JavaScript's
+	functionality wrapped in a cleaner,
+
+     },
+               image_url: '8.jpg',
+               price: 34.95)
+
+
+# . . .
+
+Product.create(title: 'Rails Test 123123Prescriptions',
+               description:
+                   %{
+        CoffeeScript is JavaScript done right. It provides all of JavaScript's
+	functionality wrapped in a cleaner,
+
+     },
+               image_url: '7.jpg',
+               price: 34.95)
+
+# . . .
+
+Product.create(title: 'Rails Test Prescriptions123',
+               description:
+                   %{
+        CoffeeScript is JavaScript done right. It provides all of JavaScript's
+	functionality wrapped in a cleaner,
+
+     },
+               image_url: '8.jpg',
                price: 34.95)
