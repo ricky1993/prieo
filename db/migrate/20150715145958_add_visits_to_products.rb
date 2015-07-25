3aed8f01938cd58eb545a79a05787370c772ad1c
Byphoenix <446596010@qq.com>
2015-07-25 14:48:38 +0800

    数据库添加visits Default：0

--- db/migrate/20150715145958_add_visits_to_products.rb
+++ db/migrate/20150715145958_add_visits_to_products.rb
@@ -1,6 +1,6 @@
 class AddVisitsToProducts < ActiveRecord::Migration
   def up
-    add_column :products, :visits, :integer
+    add_column :products, :visits, :integer, default: 0
   end
   def down
     remove_column :products, :visits
